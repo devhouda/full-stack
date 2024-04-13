6e00ff5c33a07281f572eb36c495d616329dd890
DevHouda <devhouda@gmail.com>
2024-04-13 16:14:52 +0100

    part1: Course Information, step 2

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -4,6 +4,16 @@ const Header = (props) => {
 
 const Content = (props) => {
   return (
+    <div>
+      <Part part1={props.part1} exercises1={props.exercises1} />
+      <Part part2={props.part2} exercises2={props.exercises2} />
+      <Part part3={props.part3} exercises3={props.exercises3} />
+    </div>
+  );
+};
+
+const Part = (props) => {
+  return (
     <>
       <p>
         {props.part1} {props.exercises1}
